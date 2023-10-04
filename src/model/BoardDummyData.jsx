21e466f1bef3b7924eb88336a70f75243938ef73
yujin <yjmo@containus.kr>
2023-10-05 01:45:53 +0900

    add: 게시판 더미 데이터 용 모델 파일과 recoil 파일 추가

---
+++ src/model/BoardDummyData.jsx
@@ -0,0 +1,40 @@
+const boardData = [
+  {
+    id: 1,
+    title: '안드로이드 스튜디오 에뮬레이터 오류',
+    content:
+      '플러터로 안드로이드 스튜디오에서 에뮬레이터를 실행하면 INFO | Android emulator version 32.1.12.0',
+    user: 'nickname2',
+    categories: ['ios', 'flutter'],
+    review: 4,
+  },
+  {
+    id: 2,
+    title: '안드로이드 스튜디오 에뮬레이터 오류',
+    content:
+      '플러터로 안드로이드 스튜디오에서 에뮬레이터를 실행하면 INFO | Android emulator version 32.1.12.0',
+    user: 'nickname2',
+    categories: ['ios', 'flutter'],
+    review: 4,
+  },
+  {
+    id: 3,
+    title: '안드로이드 스튜디오 에뮬레이터 오류',
+    content:
+      '플러터로 안드로이드 스튜디오에서 에뮬레이터를 실행하면 INFO | Android emulator version 32.1.12.0',
+    user: 'nickname2',
+    categories: ['ios', 'flutter'],
+    review: 4,
+  },
+  {
+    id: 4,
+    title: '안드로이드 스튜디오 에뮬레이터 오류',
+    content:
+      '플러터로 안드로이드 스튜디오에서 에뮬레이터를 실행하면 INFO | Android emulator version 32.1.12.0',
+    user: 'nickname2',
+    categories: ['ios', 'flutter'],
+    review: 4,
+  },
+];
+
+export { boardData };
