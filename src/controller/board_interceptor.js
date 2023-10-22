b34ee89b5c8ff9ec0aa2868b9020ff4de816915a
Sui <yjmo@containus.kr>
2023-10-23 00:34:56 +0900

    add: 게시판 axios interceptor 추가

--- src/controller/board_interceptor.js
+++ src/controller/board_interceptor.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
-import {
-  getLocalStorage,
-  removeLocalStorage,
-  setLocalStorage,
-} from './localStorage';
+import { useRecoilValue } from 'recoil';
+import { userEmailState } from '../recoil/userAtom';
 
 const boardInstance = axios.create({
-  baseURL: 'url',
-  headers: process.env.REACT_APP_BOARD_SERVICE_API,
+  baseURL: '/board',
+  // headers: process.env.REACT_APP_BOARD_SERVICE_API,
 });
 
+// TODO : eamil 저장해서 받아오는 로직 구현해야 됨.
 boardInstance.interceptors.request.use((config) => {
-  const accessToken = getLocalStorage('accessToken');
+  const userEmail = useRecoilValue(userEmailState);
 
   config.headers['Content-Type'] = 'application/json';
-  config.headers['Authorization'] = `Bearer ${accessToken}`;
+  config.headers['User-Email'] = userEmail;
   return config;
 });
+
+export default boardInstance;
