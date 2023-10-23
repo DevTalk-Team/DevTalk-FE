710531a174760ac8f2416626021f246506c2966d
Sui <yjmo@containus.kr>
2023-10-23 20:52:09 +0900

    add: board 더미데이터 추가

--- src/model/BoardDummyData.jsx
+++ src/model/BoardDummyData.jsx
@@ -1,37 +1,37 @@
 const boardData = [
   {
-    id: 1,
+    postId: 1,
     title: '안드로이드 스튜디오 에뮬레이터 오류',
     content:
       '플러터로 안드로이드 스튜디오에서 에뮬레이터를 실행하면 INFO | Android emulator version 32.1.12.0',
-    user: 'nickname2',
+    userId: 'nickname2',
     categories: ['ios', 'flutter'],
     review: 4,
   },
   {
-    id: 2,
+    postId: 2,
     title: '안드로이드 스튜디오 에뮬레이터 오류',
     content:
       '플러터로 안드로이드 스튜디오에서 에뮬레이터를 실행하면 INFO | Android emulator version 32.1.12.0',
-    user: 'nickname2',
+    userId: 'nickname2',
     categories: ['ios', 'flutter'],
     review: 4,
   },
   {
-    id: 3,
+    postId: 3,
     title: '안드로이드 스튜디오 에뮬레이터 오류',
     content:
       '플러터로 안드로이드 스튜디오에서 에뮬레이터를 실행하면 INFO | Android emulator version 32.1.12.0',
-    user: 'nickname2',
+    userId: 'nickname2',
     categories: ['ios', 'flutter'],
     review: 4,
   },
   {
-    id: 4,
+    postId: 4,
     title: '안드로이드 스튜디오 에뮬레이터 오류',
     content:
       '플러터로 안드로이드 스튜디오에서 에뮬레이터를 실행하면 INFO | Android emulator version 32.1.12.0',
-    user: 'nickname2',
+    userId: 'nickname2',
     categories: ['ios', 'flutter'],
     review: 4,
   },
