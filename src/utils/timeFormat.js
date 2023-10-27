0fbefa7196212e41519ea61bc0ff42c6becfc2db
Sui <yjmo@containus.kr>
2023-10-27 19:25:05 +0900

    add: 년-월-일 까지만 return 해주는 formatt 함수 추가

--- src/utils/timeFormat.js
+++ src/utils/timeFormat.js
@@ -10,3 +10,14 @@ export const changeTimeFormat = (time) => {
   const formattedDate = `${year}.${month}.${day} ${hours}:${minutes}`;
   return formattedDate;
 };
+
+export const changeTimeFormatDay = (time) => {
+  const dateObject = new Date(time);
+
+  const year = dateObject.getFullYear();
+  const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
+  const day = dateObject.getDate().toString().padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+};
