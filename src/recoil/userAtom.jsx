e87cdb25c228552da5b0c1ecb966f9dd97b1b53f
Sui <yjmo@containus.kr>
2023-10-27 19:24:31 +0900

    add: userId를 저장하기 위한 atom 생성

--- src/recoil/userAtom.jsx
+++ src/recoil/userAtom.jsx
@@ -9,14 +9,23 @@ const { persistAtom } = recoilPersist({
   storage: sessionStorage,
 });
 
+// 사용자 email 정보 atom
 export const userEmailState = atom({
   key: 'userEmailState',
   default: '', // 초기값은 빈 문자열
   effects_UNSTABLE: [persistAtom],
 });
 
+// 사용자 이름 정보 atom
 export const userNameState = atom({
   key: 'userNameState',
+  default: '', // 초기값은 빈 문자열
+  effects_UNSTABLE: [persistAtom],
+});
+
+// 사용자 Id 정보 atom
+export const userIdState = atom({
+  key: 'userIdState',
   default: '', // 초기값은 빈 문자열
   effects_UNSTABLE: [persistAtom],
 });
